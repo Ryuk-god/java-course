48caf3b882de6f3341c8d9224c3bbb821dcb0db0
Ryuk-god <vickyyadav7702@gmail.com>
2026-08-28 19:13:39 +0000

    29 Aug 2026, 00:43

---
+++ strings.java
@@ -0,0 +1,15 @@
+public class strings {
+
+    public static void main(String[] args) {
+
+        String name = new String("Navin");
+
+        System.out.println(name);
+
+        System.out.println(name.hashCode());
+
+        System.out.println("hello " + name);
+
+        System.out.println(name.concat("reddy"));
+    }
+}
