c4d561ec76437d77712901a96baa19d6f2c75fad
Ryuk-god <vickyyadav7702@gmail.com>
2026-08-28 16:59:13 +0000

    28 Aug 2026, 22:29

--- multArray.java
+++ multArray.java
@@ -1,14 +1,23 @@
-public  class multArray{
+public class multArray {
   public static void main(String[] args) {
-      int nums[][]= new int[3][];
-      // for the random value 
-      // int random = (int)Math.random()*100;
+    int nums[][] = new int[3][];
+    // for the random value
+    // int random = (int)Math.random()*100;
 
-      for(int n[]:nums){
-        for(int j :n){
-         
-        }
-        System.out.println("");
+    for (int n[] : nums) {
+      for (int j = 0; j < n.length; j++) {
+        n[j] = (int) Math.random() * 100;
       }
+      System.out.println();
+    }
+    for (int n[] : nums) {
+      for (int m : n) {
+        System.out.println(m + " ");
+      }
+      System.err.println();
+    }
   }
+
+  // System.out.println("random values of math for 2 digits || for one digit multi
+  // by 10 :" + (int)Math.random()*100);
 }
