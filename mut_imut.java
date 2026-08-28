f2b73472c90b99e5e77f63d83ade868855fa7ca7
Ryuk-god <vickyyadav7702@gmail.com>
2026-08-28 19:25:12 +0000

    29 Aug 2026, 00:55

--- mut_imut.java
+++ mut_imut.java
@@ -1,10 +1,14 @@
 public class mut_imut {
   public static void main(String[] args) {
       //mutable -> can be changed
+    
       String s =new String("Vicky");
-      s=s.concat("hello"+s);
+      s=s.concat("Yadav");
+
+      // s=s.concat("hello"+s);
       System.out.println(s);
-      s.concat("yadav");
-      System.out.println(s);
+      // s.concat("yadav");  // new String created 
+      // s=s.append("yadav");
+      // System.out.println(s);
   }
 }
