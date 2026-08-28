c3d68be50d3a8e1196a3a2fa4800e3653cd065f9
Ryuk-god <vickyyadav7702@gmail.com>
2026-08-28 16:53:45 +0000

    28 Aug 2026, 22:23

---
+++ multArray.java
@@ -0,0 +1,14 @@
+public  class multArray{
+  public static void main(String[] args) {
+      int nums[][]= new int[3][];
+      // for the random value 
+      // int random = (int)Math.random()*100;
+
+      for(int n[]:nums){
+        for(int j :n){
+         
+        }
+        System.out.println("");
+      }
+  }
+}
