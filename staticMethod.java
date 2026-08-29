f25c45d1a28fef368b15cb9b3bec7784a0546dc7
Ryuk-god <vickyyadav7702@gmail.com>
2026-08-29 08:47:52 +0000

    29 Aug 2026, 14:17

---
+++ staticMethod.java
@@ -0,0 +1,5 @@
+// A static variable is a variable declared using the static keyword. It belongs to the class rather than to individual objects, so only one copy is created and shared by all objects of that class.
+
+public class staticMethod {
+  
+}
