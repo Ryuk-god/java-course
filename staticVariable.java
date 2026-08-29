a9ed819db50a923b177abd207dd28aa942143f1f
Ryuk-god <vickyyadav7702@gmail.com>
2026-08-29 08:40:29 +0000

    29 Aug 2026, 14:10

---
+++ staticVariable.java
@@ -0,0 +1,34 @@
+// make a static variable so we cannot define to each object personally the things
+class mobile{
+  String brand;
+  int price;
+
+  static String name;
+
+  public void show(){
+    System.out.println("Brand: "+brand + " Price: "+price + " " +name);
+  }
+}
+
+public class staticVariable {
+  public static void main(String[] args) {
+
+    mobile.name = "smartphone";
+      mobile obj1= new mobile();
+      obj1.brand ="APPLE";
+      obj1.price = 2000;
+      obj1.name = "smartphone";
+
+      mobile obj2= new mobile();
+      obj2.brand = "One plus";
+      obj2.price = 1200;
+      obj2.name = "+1";
+
+      mobile.name = "smartphone";
+
+      // these prove the last static variable is give 
+      obj1.show();
+      obj2.show();
+
+  }
+}
